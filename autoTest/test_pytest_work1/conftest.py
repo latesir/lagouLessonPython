f974e8a82c037ff0a0e2d4b2661365794ae60c31
LAPTOP-9665FDKT\小13 <hui.dou@topideal.com.cn>
2020-06-29 03:07:13 +0800

    提交 - pytest作业2 课后作业1 1、使用参数化数据驱动，完成加减乘除测试用例的自动生成 2、修改测试用例为check_开头，修改测试用例的执行规则，执行所有check_开头和test_开头的测试用例 课后作业2 控制测试用例顺序按照【加-减-乘-除】这个顺序执行, 减法依赖加法， 除法依赖乘法 课后作业3 注册一个命令行参数env,env默认值是test,表示测试环境，另外还有两个值 （dev,st）不同的环境读取不同的数据。

--- autoTest/test_pytest_work1/conftest.py
+++ autoTest/test_pytest_work1/conftest.py
@@ -9,3 +9,17 @@ import pytest
 def setup_and_teardown():
     yield print('开始计算')
     print('计算结束')
+
+def pytest_addoption(parser):
+    mygroup = parser.getgroup("douhui")
+    mygroup.addoption("--env",default='test',dest='env',help='douhui add option test')
+
+@pytest.fixture(scope='session')
+def cmdoption(request):
+    myenv = request.config.getoption("--env",default='test')
+    if myenv == 'test':
+        print('获取测试环境配置信息')
+    elif myenv == 'dev':
+        print('获取开发环境配置信息')
+    elif myenv == 'st':
+        print('获取st环境配置信息')
--- autoTest/test_pytest_work1/test_calculator.py
+++ autoTest/test_pytest_work1/test_calculator.py
@@ -3,8 +3,15 @@
 # Author: latesir
 # Date: 2020/6/21
 import pytest
+import yaml
 
 from test_pytest_work1.calculator import Calculator
+
+
+def get_yaml_data(key):
+    with open('./calculator.yaml', 'r') as f:
+        datas = yaml.safe_load(f)
+        return datas[key]
 
 
 class TestCalculator:
@@ -12,20 +19,28 @@ class TestCalculator:
         print('实例化计算器类')
         self.cal = Calculator()
 
-    @pytest.mark.parametrize(['a', 'b', 'c'], [(1, 2, 3), (-1, -2, -3), (0, 0, 0), (123, 999, 1122), (-1, 2, 1)])
+    @pytest.mark.run(order=1)
+    @pytest.mark.dependency(name='add')
+    @pytest.mark.parametrize(['a', 'b', 'c'], get_yaml_data('add_sub'))
     def test_add(self, a, b, c, setup_and_teardown):
         assert c == self.cal.add(a, b)
 
-    @pytest.mark.parametrize(['c', 'b', 'a'], [(1, 2, 3), (-1, -2, -3), (0, 0, 0), (123, 999, 1122), (-1, 2, 1)])
-    def test_sub(self, a, b, c, setup_and_teardown):
+    @pytest.mark.run(order=2)
+    @pytest.mark.dependency(depends=["add"])
+    @pytest.mark.parametrize(['c', 'b', 'a'], get_yaml_data('add_sub'))
+    def check_sub(self, a, b, c, setup_and_teardown):
         assert c == self.cal.sub(a, b)
 
-    @pytest.mark.parametrize(['a', 'b', 'c'], [(1, 2, 2), (-1, -2, 2), (0, 0, 0), (120, 120, 14400), (-1, 2, -2)])
-    def test_mul(self, a, b, c, setup_and_teardown):
+    @pytest.mark.run(order=3)
+    @pytest.mark.dependency(name='mul')
+    @pytest.mark.parametrize(['a', 'b', 'c'], get_yaml_data('mul_div'))
+    def check_mul(self, a, b, c, setup_and_teardown):
         assert c == self.cal.mul(a, b)
 
-    @pytest.mark.parametrize(['a', 'b', 'c'], [(2, 1, 2), (-2, -1, 2), (0, 0, 0), (14400, 120, 120), (-2, 2, -1)])
-    def test_div(self, a, b, c, setup_and_teardown):
+    @pytest.mark.run(order=4)
+    @pytest.mark.dependency(depends=["mul"])
+    @pytest.mark.parametrize(['c', 'b', 'a'], get_yaml_data('mul_div'))
+    def check_div(self, a, b, c, setup_and_teardown):
         try:
             c == self.cal.div(a, b)
         except ZeroDivisionError:
